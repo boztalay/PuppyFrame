2cf69041e85c5ce548a01eff4e758ba7f8e36f3a
Ben Oztalay <bmoztalay@tabusnine.com>
2013-05-08 00:22:02 -0400

    Got a skeleton widget working, which displays the icon

---
+++ src/com/boztalay/picturewidget/configuration/PictureWidgetConfigurationActivity.java
@@ -0,0 +1,50 @@
+package com.boztalay.picturewidget.configuration;
+
+import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+
+import com.boztalay.picturewidget.R;
+
+public class PictureWidgetConfigurationActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_configuration);
+
+		int appWidgetId = getAppWidgetId();
+		Intent configurationResult = createConfigurationResultIntent(appWidgetId);
+
+		updateAppWidget(appWidgetId);
+		
+		setResult(RESULT_OK, configurationResult);
+		finish();
+	}
+	
+	private int getAppWidgetId() {
+		Intent intent = getIntent();
+		Bundle extras = intent.getExtras();
+		if(extras == null) {
+			throw new RuntimeException(getString(R.string.no_appwidget_id_error));
+		}
+
+		int appWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+		return appWidgetId;
+	}
+
+	private Intent createConfigurationResultIntent(int appWidgetId) {
+		Intent configurationResult = new Intent();
+		configurationResult.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+
+		return configurationResult;
+	}
+	
+	private void updateAppWidget(int appWidgetId) {
+		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+		RemoteViews views = new RemoteViews(getPackageName(), R.layout.picture_widget);
+		appWidgetManager.updateAppWidget(appWidgetId, views);
+	}
+}
---
+++ src/com/boztalay/picturewidget/widget/PictureWidgetProvider.java
@@ -0,0 +1,7 @@
+package com.boztalay.picturewidget.widget;
+
+import android.appwidget.AppWidgetProvider;
+
+public class PictureWidgetProvider extends AppWidgetProvider {
+
+}
